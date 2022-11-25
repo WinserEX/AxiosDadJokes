78af8a61179fae9b76b478b4909da38b98cff388
Winser Espinal <Wespinal@vivaresorts.com>
2022-11-25 17:54:59 -0400

    Added a button that calls a new joke from the API whenever it is pushed

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const jokes = document.querySelector('#jokes')
-const joke = async () => {
-    const res2 = await axios.get('https://v2.jokeapi.dev/joke/Programming?type=single');
-    console.log(res2.data.joke); 
+const jokes = document.querySelector('#jokes');
+const button = document.querySelector('button');
+
+
+const addJoke = async () => {
+    const jokeText = await getJoke()
+    const newLI = document.createElement('LI');
+    newLI.append(jokeText);
+    jokes.append(newLI);
+}
+
+const getJoke = async () => {
+    try {
+        const res2 = await axios.get('https://v2.jokeapi.dev/joke/Programming?type=single');
+        return res2.data.joke;
+    } 
+    catch (e) {
+        return "No Jokes Available! Sorry :("
+    }
 } 
 
-joke();
+button.addEventListener('click', addJoke);
